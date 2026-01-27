283ed075a7716821650b3f4d4e8f83e0fe045698
Xavier Macias <xavier@debian.personal.com>
2026-01-27 17:55:47 -0500

    Nueva ruta /sendmoney

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import { PayBlock } from "@/components/Pay";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-y-3">
-      <h1 className="text-2xl font-bold mb-6">Mini App de Pagos</h1>
-      <PayBlock />
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Bienvenido</h1>
     </main>
   );
 }
